b484456f4509cbae899930dcafd58583e65d4b1c
suvrajit1998 <suvrajitnaskar1998@gmail.com>
2019-11-23 10:38:11 +1400

    Updates

---
+++ precache-manifest.74a5ff0f4eb6a2729c9e7f569033ab53.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5e3d94caf9dd84cfc589c8f92bfdc89e",
+    "url": "/Monsters-Rolodex/index.html"
+  },
+  {
+    "revision": "ff1e471e7b0eafd422cf",
+    "url": "/Monsters-Rolodex/static/css/main.2151adc8.chunk.css"
+  },
+  {
+    "revision": "461d680aaf6f04d410d8",
+    "url": "/Monsters-Rolodex/static/js/2.4e8a168c.chunk.js"
+  },
+  {
+    "revision": "ff1e471e7b0eafd422cf",
+    "url": "/Monsters-Rolodex/static/js/main.b415f88c.chunk.js"
+  },
+  {
+    "revision": "686dd1e89d894be440de",
+    "url": "/Monsters-Rolodex/static/js/runtime-main.ddca5747.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/Monsters-Rolodex/static/media/logo.25bf045c.svg"
+  }
+]);
